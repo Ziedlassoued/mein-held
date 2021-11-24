5cdbe26f51efb95ee580df3042b9d85fc3c14338
ziedlassoued <zied.lassoued@outlook.de>
2021-11-24 15:36:04 +0100

    :sparkles: Prepare styleguide

---
+++ src/Styleguide.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Styleguide() {
+  return (
+    <div>
+      <h1>Headline 1</h1>
+      <h2>Headline 2</h2>
+      <h3>Headline 3</h3>
+    </div>
+  );
+}
+export default Styleguide;
