8120b82ce33076afd4d472a228a6ba38071e1836
ziedlassoued <zied.lassoued@outlook.de>
2021-11-30 17:49:56 +0100

    :lipstick: Add styling for welcome section

--- src/components/Welcome/WelcomeSection.tsx
+++ src/components/Welcome/WelcomeSection.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import styles from './WelcomeSection.module.css';
 
-function WelcomeSection() {
-  return <div className={styles.container}>Welcome</div>;
+function WelcomeSection(): JSX.Element {
+  return (
+    <section className={styles.welcome}>
+      <button>Jetzt die besten Handwerker finden</button>
+      <div className={styles.container}>
+        <img
+          src="src/assets/Handwerker_gesucht.jpeg"
+          alt="handwerker gesucht"
+        />
+        <div>
+          <img src="src/assets/Handwerker_welcome.jpeg" alt="welcome" />
+          <button>Partner werden</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default WelcomeSection;
