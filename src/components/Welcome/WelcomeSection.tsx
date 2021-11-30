ae015fab0cb42bcf5d53a34b61e9af6a3fc724af
ziedlassoued <zied.lassoued@outlook.de>
2021-11-30 17:04:10 +0100

    :sparkles: Add Welcome section

---
+++ src/components/Welcome/WelcomeSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './WelcomeSection.module.css';
+
+function WelcomeSection() {
+  return <div className={styles.container}>Welcome</div>;
+}
+export default WelcomeSection;
