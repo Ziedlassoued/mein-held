0aa825845342ec442bc3bf799346879ce8e99b3a
ziedlassoued <zied.lassoued@outlook.de>
2021-12-01 15:23:48 +0100

    :sparkles: Add NavBar & Footer to services pages

--- src/pages/GaertnerList/GaertnerList.tsx
+++ src/pages/GaertnerList/GaertnerList.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function GaertnerList() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
 }
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -4,14 +4,29 @@ import NavBar from '../../components/NavBar/NavBar';
 import Registration from '../../components/Registration/Registration';
 import SelectServices from '../../components/SelectServices/SelectServices';
 import WelcomeSection from '../../components/Welcome/WelcomeSection';
+import styles from './LandingPage.module.css';
 function LandingPage() {
   return (
-    <div>
-      <NavBar />
-      <WelcomeSection />
-      <SelectServices />
-      <Registration />
-      <Footer />
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <section className={styles.navBar}>
+          <NavBar />
+        </section>
+        <section className={styles.welcomeSection}>
+          <WelcomeSection />
+        </section>
+      </div>
+      <section className={styles.selectServices}>
+        <SelectServices />
+      </section>
+      <div className={styles.out}>
+        <section className={styles.registration}>
+          <Registration />
+        </section>
+        <section className={styles.footer}>
+          <Footer />
+        </section>
+      </div>
     </div>
   );
 }
