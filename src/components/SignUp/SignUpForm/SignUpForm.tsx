5d7b416bd3f7bd708db75de67256396dda5bb2ee
ziedlassoued <zied.lassoued@outlook.de>
2021-12-09 15:29:02 +0100

    :heavy_plus_sign: Add schema-based form validation with Yup to react hook form

--- src/components/SignUp/SignUpForm/SignUpForm.tsx
+++ src/components/SignUp/SignUpForm/SignUpForm.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { Link } from 'react-router-dom';
 import Footer from '../../Footer/Footer';
 import NavBar from '../../NavBar/NavBar';
@@ -8,17 +10,46 @@ import styles from './SignUpForm.module.css';
 interface FormInputProps {
   companyName: string;
   owner: string;
+  file: File;
   email: string;
   street: string;
-  houseNr: number;
+  houseNumber: number;
   zip: number;
   city: string;
   phonNumber: number;
   category: string;
+  password: string;
+  confirmPassword: string;
 }
 
+const schema = yup.object().shape({
+  companyName: yup.string().required('bitte Firmen Name eingeben'),
+  category: yup.string().required(),
+  owner: yup
+    .string()
+    .required('bitte Vor- und Nachname der Firmen Inhaber eingeben'),
+  street: yup.string().required('bitte Straße eingeben'),
+  city: yup.string().required('bitte Stadt eingeben'),
+  zip: yup.number().positive().integer().min(5).required('bitte plz eingeben'),
+  email: yup.string().email().required('bitte Email eingeben'),
+  houseNumber: yup
+    .number()
+    .positive()
+    .integer()
+    .required('bitte Haus nr. eingeben'),
+  password: yup.string().min(4).max(15).required(),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null]),
+  file: yup.mixed().required('Firmen Logo'),
+});
+
 export default function SignUpForm() {
-  const { register, handleSubmit } = useForm<FormInputProps>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputProps>({
+    resolver: yupResolver(schema),
+  });
   const onSubmit: SubmitHandler<FormInputProps> = (data) => {
     console.log(data);
     fetch('/api/users', {
@@ -43,6 +74,7 @@ export default function SignUpForm() {
               <div className={styles.inputBox}>
                 <label className={styles.details}>Firmen Name</label>
                 <input {...register('companyName')} />
+                <p>{errors.companyName?.message}</p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Kategorie auswählen</label>
@@ -61,14 +93,17 @@ export default function SignUpForm() {
                   <option value="Maurer">Maurer</option>
                   <option value="Bodenleger">Bodenleger</option>
                 </select>
+                <p> {errors.category?.message} </p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Inhaber</label>
                 <input {...register('owner')} />
+                <p>{errors.owner?.message}</p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Email</label>
                 <input {...register('email')} />
+                <p> {errors.email?.message} </p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Telefon Nr.</label>
@@ -77,18 +112,51 @@ export default function SignUpForm() {
               <div className={styles.inputBox}>
                 <label className={styles.details}>Straße</label>
                 <input {...register('street')} />
+                <p> {errors.street?.message} </p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Haus Nr.</label>
-                <input {...register('houseNr')} />
+                <input {...register('houseNumber')} />
+                <p> {errors.houseNumber?.message} </p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Postleitzahl</label>
                 <input {...register('zip')} />
+                <p> {errors.zip?.message} </p>
               </div>
               <div className={styles.inputBox}>
                 <label className={styles.details}>Stadt</label>
                 <input {...register('city')} />
+                <p> {errors.city?.message} </p>
+              </div>
+              <div className={styles.inputBox}>
+                <label className={styles.details}>Firmen Logo</label>
+                <input
+                  className={styles.inputLogo}
+                  type="file"
+                  {...register('file')}
+                />
+                <p> {errors.file?.message}</p>
+              </div>
+              <div className={styles.inputBox}>
+                <span className={styles.details}>Passwort</span>
+                <input
+                  type="password"
+                  placeholder="Passwort"
+                  {...register('password')}
+                />
+                <p> {errors.password?.message} </p>
+              </div>
+              <div className={styles.inputBox}>
+                <span className={styles.details}>Passwort bestätigen</span>
+                <input
+                  type="password"
+                  placeholder="Passwort bestätigen"
+                  {...register('confirmPassword')}
+                />
+                <p>
+                  {errors.confirmPassword && 'Passwort muss übereinstimmen!'}
+                </p>
               </div>
               <div className={styles.btn}>
                 <input type="submit" value="Anmelden" />
