682f1df9f23072ee7163d9dfb2b4f7f4800bbd35
ziedlassoued <zied.lassoued@outlook.de>
2021-12-14 17:07:07 +0100

    :heavy_plus_sign: Install redux and redux toolkit

--- src/components/WelcomeSecond/WelcomeSecond.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './WelcomeSecond.module.css';
-
-function WelcomeSecond() {
-  return <div className={styles.container}>hello wolrd</div>;
-}
-
-export default WelcomeSecond;
